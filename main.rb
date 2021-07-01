584dc8013920366c9f49d2786b2313f7bc86d532
RaghapBean <69561650+PandaBean18@users.noreply.github.com>
2021-07-01 23:11:30 +0530

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,3 +1,7 @@
+#this is the main file that u need to run. keep solver, input.txt and this file in the same folder. input is an empty file, i have used it to store the input and 
+#convert it into an array. i have also made it so after each run it will delete all the contents in the input.txt file as they are not required and will only come
+#in the way if you decide to run it again.
+
 require_relative "solver.rb"
 def ask_user
     puts "please paste the maze. add 1 tab of space (4 spaces) in the last line"
